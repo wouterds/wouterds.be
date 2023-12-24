5d6271bf8e17e43e454aa2fdbe485ae4319c6b4c
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2023-12-24 17:45:54 +0100

    feat: handle errors

--- src/root.tsx
+++ src/root.tsx
@@ -1,12 +1,14 @@
 import type { LinksFunction } from '@remix-run/cloudflare';
 import { cssBundleHref } from '@remix-run/css-bundle';
 import {
+  isRouteErrorResponse,
   Links,
   LiveReload,
   Meta,
   Outlet,
   Scripts,
   ScrollRestoration,
+  useRouteError,
 } from '@remix-run/react';
 
 import stylesheet from '~/tailwind.css';
@@ -45,3 +47,43 @@ export default function App() {
     </html>
   );
 }
+
+export const ErrorBoundary = () => {
+  const error = useRouteError();
+
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <div className="mx-auto mt-2" style={{ maxWidth: '768px' }}>
+          <Header />
+
+          <main className="my-12">
+            {isRouteErrorResponse(error) ? (
+              <h1 className="text-xl font-medium">
+                {error.status} {error.statusText}
+              </h1>
+            ) : error instanceof Error ? (
+              <>
+                <h1 className="text-xl font-medium mb-2">Error</h1>
+                <p>{error.message}</p>
+              </>
+            ) : (
+              <h1 className="text-xl font-medium">Unknown error</h1>
+            )}
+          </main>
+
+          <Footer />
+        </div>
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+  );
+};
