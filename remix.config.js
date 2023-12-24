bfbd3015b01abf81ab7b517a1a87ca04718b4426
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2023-12-24 14:36:29 +0100

    chore: sync with basic remix 2.4.1 project

--- remix.config.js
+++ remix.config.js
@@ -1,12 +1,11 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 export default {
   ignoredRouteFiles: ["**/.*", "**/*.css"],
-  server: "./src/server.ts",
+  server: "src/server.ts",
   serverBuildPath: "functions/[[path]].js",
   serverConditions: ["workerd", "worker", "browser"],
   serverDependenciesToBundle: "all",
   serverMainFields: ["browser", "module", "main"],
-  serverMinify: true,
   serverModuleFormat: "esm",
   serverPlatform: "neutral",
   appDirectory: "src",
