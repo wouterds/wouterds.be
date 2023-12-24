a949ff0c79eb9454d3ffee169bda1fdb4c35628c
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2023-12-24 15:41:50 +0100

    feat: page generated in

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,14 @@
-const Footer = () => (
-  <footer className="border-t border-dashed border-zinc-900 dark:border-zinc-100 pt-3">
-    <p>&copy; {new Date().getFullYear()} Wouter De Schuyter</p>
-  </footer>
-);
+interface Props {
+  time: number;
+}
+
+const Footer = ({ time }: Props) => {
+  return (
+    <footer className="border-t border-dashed border-zinc-900 dark:border-zinc-100 pt-3 flex justify-between">
+      <p>&copy; {new Date().getFullYear()} Wouter De Schuyter</p>
+      <p>generated in {Date.now() - time}ms</p>
+    </footer>
+  );
+};
 
 export default Footer;
