cddfa643e78a18d67be4c1a91811a3de28954c12
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2023-12-25 10:44:58 +0100

    chore:: codegen script

---
+++ codegen.ts
@@ -0,0 +1,44 @@
+import { CodegenConfig } from '@graphql-codegen/cli';
+import dotenv from 'dotenv';
+
+dotenv.config({ path: '.dev.vars' });
+
+const config: CodegenConfig = {
+  schema: [
+    {
+      [process.env.DATOCMS_API_ENDPOINT as string]: {
+        headers: {
+          Authorization: `Bearer ${process.env.DATOCMS_API_KEY as string}`,
+        },
+      },
+    },
+  ],
+  documents: ['./src/graphql/**/*.graphql'],
+  generates: {
+    './src/graphql/types.ts': {
+      plugins: [
+        'typescript',
+        {
+          'typescript-operations': {
+            strictScalars: true,
+            scalars: {
+              BooleanType: 'boolean',
+              CustomData: 'Record<string, unknown>',
+              Date: 'string',
+              DateTime: 'string',
+              FloatType: 'number',
+              IntType: 'number',
+              ItemId: 'string',
+              JsonField: 'unknown',
+              MetaTagAttributes: 'Record<string, string>',
+              UploadId: 'string',
+            },
+          },
+        },
+        'typed-document-node',
+      ],
+    },
+  },
+};
+
+export default config;
