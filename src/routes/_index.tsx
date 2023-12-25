2b326859a24ebfbb11677fc4b13a0b0d4a7546ab
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2023-12-25 13:03:17 +0100

    refactor: always explicitly init per loader

--- src/routes/_index.tsx
+++ src/routes/_index.tsx
@@ -4,8 +4,13 @@ import { Posts } from '~/components/Posts';
 import { PostRecord } from '~/graphql';
 import { PostRepository } from '~/repositories/post.server';
 
-export const loader = async () => {
-  const posts = await PostRepository.getAll();
+export const loader = async (args: LoaderFunctionArgs) => {
+  const repository = new PostRepository(
+    args.context.env.DATOCMS_API_URL,
+    args.context.env.DATOCMS_API_KEY,
+  );
+
+  const posts = await repository.getAll();
 
   return { posts };
 };
