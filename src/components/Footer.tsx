3d337268956d1a815d6b299232de9ce08f6b72ae
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2023-12-25 13:10:11 +0100

    fix: remove footer border

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="border-t border-b border-dashed border-zinc-900 dark:border-zinc-100 py-3 flex justify-between">
+    <footer className="border-t border-dashed border-zinc-900 dark:border-zinc-100 py-3 flex justify-between">
       <p>&copy; {new Date().getFullYear()} Wouter De Schuyter</p>
     </footer>
   );
