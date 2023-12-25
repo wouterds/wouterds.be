21554e9cefd7c1ce9563e51dfb329dc78c55ef58
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2023-12-25 14:52:58 +0100

    feat: detail blog page -- wip

--- src/services/datocms.server.ts
+++ src/services/datocms.server.ts
@@ -1,13 +1,17 @@
 import { ASTNode, print } from 'graphql';
 
-export const fetchFromDato = async <TData>(
+export const fetchFromDato = async <TData, TVariables = Record<string, string>>(
   query: ASTNode,
-  options: { apiEndpoint: string; apiKey: string },
+  options: {
+    apiEndpoint: string;
+    apiKey: string;
+    variables?: TVariables;
+  },
 ) => {
   const response = await fetch(options.apiEndpoint, {
     method: 'POST',
     headers: { Authorization: `Bearer ${options.apiKey}` },
-    body: JSON.stringify({ query: print(query) }),
+    body: JSON.stringify({ query: print(query), variables: options.variables }),
   });
 
   const { data } = await response.json<{ data: TData }>();
