25795728cb7b153e4816df9ab2e451fcce36d391
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2023-12-25 07:58:56 +0100

    feat: fetch posts from DatoCMS

---
+++ src/lib/env.server.ts
@@ -0,0 +1,8 @@
+export const extractDatocmsApiKey = (context: Record<string, unknown>) => {
+  const env = context.env as Record<string, string>;
+  if (!env?.DATOCMS_API_KEY) {
+    throw new Error('Missing DATOCMS_API_KEY');
+  }
+
+  return env.DATOCMS_API_KEY;
+};
