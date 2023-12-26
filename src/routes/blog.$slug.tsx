2d09aa4de391377e8339b74e734e3a66c09efca0
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2023-12-26 13:07:16 +0100

    feat: render posts

--- src/routes/blog.$slug.tsx
+++ src/routes/blog.$slug.tsx
@@ -1,7 +1,13 @@
 import { LoaderFunctionArgs, MetaFunction } from '@remix-run/cloudflare';
 import { useLoaderData } from '@remix-run/react';
+import {
+  RenderBlockContext,
+  StructuredText,
+  StructuredTextDocument,
+} from 'react-datocms';
 
 import { Context } from '~/@types';
+import { PostGalleryRecord } from '~/graphql';
 import { PostRepository } from '~/lib/repositories/post.server';
 
 export const loader = async (args: LoaderFunctionArgs) => {
@@ -30,8 +36,31 @@ export default function BlogSlug() {
   const { post } = useLoaderData<typeof loader>();
 
   return (
-    <article>
+    <article className="prose prose-sm max-w-none text-xs">
       <h1 className="text-2xl">{post.title}</h1>
+
+      <StructuredText
+        data={post.content as unknown as StructuredTextDocument}
+        renderBlock={renderBlock}
+      />
     </article>
   );
 }
+
+export const renderBlock = ({
+  record,
+}: RenderBlockContext<PostGalleryRecord & { __typename: string }>) => {
+  if (record.__typename === 'PostGalleryRecord') {
+    return (
+      <ul className="not-prose flex flex-col gap-2 mt-4">
+        {record.images.map((image) => (
+          <li key={`post-gallery.image:${image.id}`}>
+            <img src={image.url} alt={image.alt || undefined} />
+          </li>
+        ))}
+      </ul>
+    );
+  }
+
+  return null;
+};
