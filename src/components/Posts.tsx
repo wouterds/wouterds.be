0ba7febcfcebdaf572b525c2465616945798fb95
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2023-12-26 23:33:32 +0100

    feat: more subtle color for overview posts

--- src/components/Posts.tsx
+++ src/components/Posts.tsx
@@ -12,7 +12,9 @@ export const Posts = ({ posts }: PostsProps) => {
           <h3 className="text-base font-medium mb-2">
             <a href={`/blog/${post.slug}`}>{post.title}</a>
           </h3>
-          <p className="leading-relaxed line-clamp-2">{post.excerpt}</p>
+          <p className="leading-relaxed line-clamp-2 text-zinc-700 dark:text-zinc-300">
+            {post.excerpt}
+          </p>
         </li>
       ))}
     </ul>
