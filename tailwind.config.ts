2127f068a7e344a8a2890f3cf2802af7a0487d57
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2023-12-26 13:05:19 +0100

    chore: configure tailwind typography plugin

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,23 @@ import type { Config } from 'tailwindcss';
 export default {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      typography: (_theme: string) => ({
+        DEFAULT: {
+          css: {
+            h1: {
+              fontWeight: '600',
+            },
+            h2: {
+              fontWeight: '600',
+            },
+            h3: {
+              fontWeight: '600',
+            },
+          },
+        },
+      }),
+    },
   },
-  plugins: [],
+  plugins: [require('@tailwindcss/typography')],
 } satisfies Config;
