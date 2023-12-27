eec85d322c0f2393139617f3f3186e2b458d4970
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2023-12-27 13:51:01 +0100

    feat: avatar easter egg

--- src/routes/_index.tsx
+++ src/routes/_index.tsx
@@ -23,11 +23,18 @@ export default function Index() {
   return (
     <>
       <header className="text-center mb-14">
-        <img
-          src="/images/wouterds.2024.jpg"
-          alt="Headshot of Wouter De Schuyter"
-          className="rounded-full w-32 h-32 mx-auto mb-4 bg-zinc-100 dark:bg-zinc-800 dark:bg-opacity-50"
-        />
+        <div className="relative overflow-hidden rounded-full w-32 h-32 mx-auto mb-4 bg-zinc-100 dark:bg-zinc-800 dark:bg-opacity-50">
+          <img
+            src="/images/wouterds-2024.jpg"
+            alt="Headshot of Wouter De Schuyter"
+            className="position absolute inset-0 w-full h-full"
+          />
+          <img
+            src="/images/wouterds-2024-spectrum.jpg"
+            alt="Headshot of Wouter De Schuyter"
+            className="position absolute inset-0 w-full h-full opacity-0 hover:opacity-100"
+          />
+        </div>
         <h1 className="text-2xl font-medium mb-2">Wouter De Schuyter</h1>
         <h2 className="text-black dark:text-white text-opacity-50 dark:text-opacity-50">
           Digital Creative & Developer
