499a87499424fa4b219c928251167c2205ecf940
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2023-12-27 13:27:41 +0100

    feat: optional close icon for ux purposes

--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -1,3 +1,4 @@
+import { XMarkIcon } from '@heroicons/react/24/solid';
 import { useState } from 'react';
 import { createPortal } from 'react-dom';
 
@@ -23,10 +24,15 @@ export const Image = ({ url, alt }: ImageProps) => {
         typeof document !== 'undefined' &&
         createPortal(
           <div
-            className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-95 cursor-pointer p-6"
+            className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-95 p-6"
             onClick={() => setExpanded(false)}>
+            <button
+              className="absolute top-4 right-4 p-1 text-white rounded hover:bg-zinc-300 hover:bg-opacity-10"
+              onClick={() => setExpanded(false)}>
+              <XMarkIcon className="w-6 h-6" />
+            </button>
             <img
-              className="max-w-full max-h-full pointer-events-none bg-zinc-50 dark:bg-zinc-800 dark:bg-opacity-25"
+              className="max-w-full max-h-full bg-zinc-50 dark:bg-zinc-800 dark:bg-opacity-25"
               src={`/images${new URL(url).pathname}`}
               alt={alt || undefined}
             />
