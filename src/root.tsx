eb0d26e454f05eb2e1986a41dbae8851ac2f47a0
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2023-12-28 15:43:45 +0100

    feat: favicon

--- src/root.tsx
+++ src/root.tsx
@@ -32,6 +32,19 @@ export default function App() {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="icon" type="image/x-icon" href="favicon.ico" />
         <Meta />
         <Links />
       </head>
