7a2ab6d695293c3ce5b2c349fecbe3f6e6aff9ad
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2023-12-28 07:20:15 +0100

    chore: load react-hooks/recommended plugin

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,6 +7,7 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
     'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
     'prettier',
   ],
   plugins: ['@typescript-eslint', 'simple-import-sort', 'prettier'],
