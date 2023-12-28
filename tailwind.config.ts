9b0865a6e6762c3a1fe4f1a1e162573ebd4503c2
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2023-12-28 07:25:23 +0100

    style: improve readability

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,18 +7,10 @@ export default {
       typography: (_theme: string) => ({
         DEFAULT: {
           css: {
-            h1: {
-              fontWeight: '600',
-            },
-            h2: {
-              fontWeight: '600',
-            },
-            h3: {
-              fontWeight: '600',
-            },
-            blockquote: {
-              fontWeight: 'inherit',
-            },
+            h1: { fontWeight: '600' },
+            h2: { fontWeight: '600' },
+            h3: { fontWeight: '600' },
+            blockquote: { fontWeight: 'inherit' },
           },
         },
       }),
