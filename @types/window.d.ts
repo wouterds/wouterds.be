9ef32bf9716af3290dff634eb5728c603770e2a5
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2023-12-28 07:27:47 +0100

    chore: type shiki on window

---
+++ @types/window.d.ts
@@ -0,0 +1,12 @@
+export declare global {
+  interface Window {
+    shiki: {
+      getHighlighter: (options: {
+        theme?: string;
+        langs?: string[];
+      }) => Promise<{
+        codeToHtml: (code: string, options?: { lang?: string }) => string;
+      }>;
+    };
+  }
+}
