e5e133066f7eb9272370561b03df6b1236117204
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2023-12-28 08:26:00 +0100

    fix: tailwind overrides

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,24 @@
 import type { Config } from 'tailwindcss';
 
+const customizations = {
+  h1: { fontWeight: '600' },
+  h2: { fontWeight: '600' },
+  h3: { fontWeight: '600' },
+  blockquote: { fontWeight: 'inherit' },
+  pre: { fontSize: 'inherit' },
+};
+
 export default {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
-      typography: (_theme: string) => ({
-        DEFAULT: {
-          css: {
-            h1: { fontWeight: '600' },
-            h2: { fontWeight: '600' },
-            h3: { fontWeight: '600' },
-            blockquote: { fontWeight: 'inherit' },
-          },
-        },
-      }),
+      typography: {
+        DEFAULT: { css: customizations },
+        sm: { css: customizations },
+        lg: { css: customizations },
+        xl: { css: customizations },
+        '2xl': { css: customizations },
+      },
     },
   },
   plugins: [require('@tailwindcss/typography')],
