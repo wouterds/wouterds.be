ff52f22b54ea31c04145ae60f5708fd4bc48ef3c
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2023-12-29 13:51:32 +0100

    feat: update contact page

--- src/routes/contact.tsx
+++ src/routes/contact.tsx
@@ -6,15 +6,23 @@ export const meta: MetaFunction = () => {
 
 export default function Contact() {
   return (
-    <p>
-      <strong>Wouter De Schuyter</strong>
-      <br />
-      email:{' '}
-      <a href="mailto:wouter.de.schuyter@gmail.com">
-        wouter.de.schuyter@gmail.com
-      </a>
-      <br />
-      tel: <a href="tel:+32479228210">+32479228210</a>
-    </p>
+    <>
+      <p className="mb-4">
+        If you&apos;re writing for a project or to work together, please include
+        as much details as possible (goal, timeline, budget, ...). For
+        everything else, write as you please, I&apos;ll be more than happy to
+        reply!
+      </p>
+      <p>
+        <strong>Wouter De Schuyter</strong>
+        <br />
+        email:{' '}
+        <a href="mailto:wouter.de.schuyter@gmail.com">
+          wouter.de.schuyter@gmail.com
+        </a>
+        <br />
+        tel: <a href="tel:+32479228210">+32479228210</a>
+      </p>
+    </>
   );
 }
