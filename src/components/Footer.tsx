a223c17017f58fbe0a1ecaa394346cc14e5e4957
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2023-12-30 15:08:33 +0100

    fix: no wrap

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,9 @@ type Props = {
 const Footer = ({ ray }: Props) => {
   return (
     <footer className="border-t border-dashed border-zinc-900 dark:border-zinc-100 py-3 flex justify-between gap-2">
-      <p>&copy; {new Date().getFullYear()} Wouter De Schuyter</p>
+      <p className="whitespace-nowrap">
+        &copy; {new Date().getFullYear()} Wouter De Schuyter
+      </p>
       {ray && <p className="truncate">{ray}</p>}
     </footer>
   );
