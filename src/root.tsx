c28c5cb283dea1b36592e729fffae7f8400b4cd3
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2023-12-31 23:30:38 +0100

    revert: beacon seems unrelated

--- src/root.tsx
+++ src/root.tsx
@@ -160,14 +160,6 @@ export default function App() {
         <LiveReload />
 
         <div id="modal-portal" />
-
-        {!data?.url?.includes('localhost') && (
-          <script
-            defer
-            src="https://static.cloudflareinsights.com/beacon.min.js"
-            data-cf-beacon='{"token": "7316ff9489bb4ad5977fa6f3e4981db1"}'
-          />
-        )}
       </body>
     </html>
   );
