e42516a7f7625e9acfb5d2d4da46c3e57891a44e
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2023-12-31 13:04:56 +0100

    feat: form elements

--- src/routes/contact.tsx
+++ src/routes/contact.tsx
@@ -20,16 +20,56 @@ export default function Contact() {
         everything else, write as you please, I&apos;ll be more than happy to
         reply!
       </p>
-      <p>
-        <strong>Wouter De Schuyter</strong>
-        <br />
-        email:{' '}
-        <a href="mailto:wouter.de.schuyter@gmail.com">
-          wouter.de.schuyter@gmail.com
-        </a>
-        <br />
-        tel: <a href="tel:+32479228210">+32479228210</a>
-      </p>
+      <form
+        action=""
+        method="post"
+        className="flex flex-col gap-4"
+        style={{ maxWidth: '640px' }}>
+        <div className="flex gap-4 flex-col sm:flex-row">
+          <div className="flex-1">
+            <label className="font-semibold inline-block mb-1" htmlFor="name">
+              Name
+            </label>
+            <input
+              className="block px-2 py-1.5 border border-gray-300 rounded-sm"
+              type="text"
+              name="name"
+              id="name"
+              required
+            />
+          </div>
+          <div className="flex-1">
+            <label className="font-semibold inline-block mb-1" htmlFor="email">
+              Email
+            </label>
+            <input
+              className="block px-2 py-1.5 border border-gray-300 rounded-sm"
+              type="email"
+              name="email"
+              id="email"
+              required
+            />
+          </div>
+        </div>
+        <div>
+          <label className="font-semibold inline-block mb-1" htmlFor="message">
+            Message
+          </label>
+          <textarea
+            className="block px-2 py-1.5 border border-gray-300 rounded-sm"
+            name="message"
+            id="message"
+            required
+          />
+        </div>
+        <div>
+          <button
+            className="inline-block px-4 py-1.5 bg-gray-900 hover:bg-black text-white font-semibold rounded-sm"
+            type="submit">
+            Submit
+          </button>
+        </div>
+      </form>
     </>
   );
 }
