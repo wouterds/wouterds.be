72d51d1affd37d981cf8214af6b4f93249b9e7a5
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2024-01-01 00:57:47 +0100

    feat: experiments link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,14 +1,24 @@
+import { Link } from '@remix-run/react';
+
 type Props = {
   ray?: string | null;
 };
 
 const Footer = ({ ray }: Props) => {
   return (
-    <footer className="border-t border-dashed border-zinc-900 dark:border-zinc-100 py-3 flex justify-between gap-2">
-      <p className="whitespace-nowrap">
-        &copy; {new Date().getFullYear()} Wouter De Schuyter
-      </p>
-      {ray && <p className="truncate">{ray}</p>}
+    <footer className="border-t border-dashed border-zinc-900 dark:border-zinc-100 py-3">
+      <div className="flex justify-between gap-2 mb-12">
+        <p>&copy; {new Date().getFullYear()} Wouter De Schuyter</p>
+        <p>
+          <Link to="/experiments">experiments</Link>
+        </p>
+      </div>
+
+      {ray && (
+        <p className="text-center text-zinc-300 dark:text-zinc-600 text-xs py-2">
+          {ray}
+        </p>
+      )}
     </footer>
   );
 };
