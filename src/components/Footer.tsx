9c418ba3bb839f636d25881c4bfc533f02d8e942
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2024-01-01 01:44:17 +0100

    feat: changed footer design a little

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,20 +6,22 @@ type Props = {
 
 const Footer = ({ ray }: Props) => {
   return (
-    <footer className="border-t border-dashed border-zinc-900 dark:border-zinc-100 py-3">
-      <div className="flex justify-between gap-2 mb-12">
-        <p>&copy; {new Date().getFullYear()} Wouter De Schuyter</p>
-        <p>
-          <Link to="/experiments">experiments</Link>
-        </p>
-      </div>
+    <>
+      <footer className="border-t border-b border-dashed border-zinc-900 dark:border-zinc-100 py-3">
+        <div className="flex justify-between gap-2">
+          <p>&copy; {new Date().getFullYear()} Wouter De Schuyter</p>
+          <p>
+            <Link to="/experiments">experiments</Link>
+          </p>
+        </div>
+      </footer>
 
       {ray && (
-        <p className="text-center text-zinc-300 dark:text-zinc-600 text-xs py-2">
+        <p className="text-center text-zinc-300 dark:text-zinc-600 py-3 mt-6">
           {ray}
         </p>
       )}
-    </footer>
+    </>
   );
 };
 
