0438a44dce44c549bd06336aea2a983bfe4e774d
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2024-05-06 23:27:36 +0200

    fix: react-use

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,9 @@ import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
+  ssr: {
+    noExternal: ['react-use'],
+  },
   plugins: [
     remixCloudflareDevProxy(),
     remix({
