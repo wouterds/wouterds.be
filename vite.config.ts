b9ece5126a7c9729cc6de40bf926502b3410a407
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2024-05-06 22:53:17 +0200

    chore: remix.config.js -> vite.config.ts

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import {
+  cloudflareDevProxyVitePlugin as remixCloudflareDevProxy,
+  vitePlugin as remix,
+} from '@remix-run/dev';
+import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+export default defineConfig({
+  plugins: [
+    remixCloudflareDevProxy(),
+    remix({
+      appDirectory: 'src',
+      future: {
+        unstable_singleFetch: true,
+      },
+    }),
+    tsconfigPaths(),
+  ],
+});
