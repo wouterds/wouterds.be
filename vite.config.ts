c164a7c027ae795eb12ec883a6098896cf6bd387
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2024-05-07 23:05:36 +0200

    fix: not relevant in this setup

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,9 @@ import {
   cloudflareDevProxyVitePlugin as remixCloudflareDevProxy,
   vitePlugin as remix,
 } from '@remix-run/dev';
-import { installGlobals } from '@remix-run/node';
 import { sentryVitePlugin } from '@sentry/vite-plugin';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
-
-installGlobals();
 
 export default defineConfig({
   server: {
