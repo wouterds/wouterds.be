d76ad50d19720707d916aa7e9b026e622289a98a
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2024-05-07 23:07:32 +0200

    style: changed code style

--- vite.config.ts
+++ vite.config.ts
@@ -7,29 +7,17 @@ import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  server: {
-    port: 3000,
-  },
-  ssr: {
-    noExternal: ['react-use'],
-  },
-  build: {
-    sourcemap: true,
-  },
+  server: { port: 3000 },
+  ssr: { noExternal: ['react-use'] },
+  build: { sourcemap: true },
   plugins: [
     remixCloudflareDevProxy(),
     remix({
       ignoredRouteFiles: ['**/*.css'],
       appDirectory: 'src',
-      future: {
-        unstable_singleFetch: true,
-      },
-    }),
-    sentryVitePlugin({
-      org: 'wouterds',
-      project: 'website',
-      silent: true,
+      future: { unstable_singleFetch: true },
     }),
     tsconfigPaths(),
+    sentryVitePlugin({ org: 'wouterds', project: 'website', silent: true }),
   ],
 });
