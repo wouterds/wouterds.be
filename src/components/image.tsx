e63a0e6cded318d40b8954ffc2d9fef90bd2465a
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2024-05-13 20:40:06 +0200

    feat: image gallery navigation
    this closes https://github.com/wouterds/wouterds.be/issues/33

--- src/components/image.tsx
+++ src/components/image.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { createPortal } from 'react-dom';
 
 import { FileField } from '~/graphql';
@@ -6,9 +6,21 @@ import { humanReadableSize } from '~/lib/utils';
 
 import Loader from './loader';
 
-export const Image = ({ url, alt, responsiveImage, width, height, size }: FileField) => {
+interface Props extends FileField {
+  images: FileField[];
+}
+
+export const Image = ({ id, images }: Props) => {
   const [expanded, setExpanded] = useState(false);
   const [loading, setLoading] = useState(false);
+  const [activeIndex, setActiveIndex] = useState(
+    images?.findIndex((image) => image.id === id) || 0,
+  );
+
+  const { url, alt, responsiveImage, width, height, size } = useMemo(
+    () => images[activeIndex]!,
+    [activeIndex, images],
+  );
 
   useEffect(() => {
     if (expanded) {
@@ -50,9 +62,71 @@ export const Image = ({ url, alt, responsiveImage, width, height, size }: FileFi
       {expanded &&
         typeof document !== 'undefined' &&
         createPortal(
-          <div
-            className="fixed inset-0 z-50 bg-black bg-opacity-95 flex"
-            onClick={() => setExpanded(false)}>
+          <div className="fixed inset-0 z-50 bg-black bg-opacity-90 backdrop-blur-sm flex">
+            <header className="absolute z-20 top-4 left-4 right-4 flex items-center justify-end">
+              <button
+                title="Close"
+                className="text-white text-opacity-90 hover:text-opacity-100 rounded-sm hover:bg-zinc-300 hover:bg-opacity-10 aspect-square w-7 h-7 text-lg inline-flex items-center justify-center"
+                onClick={() => setExpanded(false)}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  height="1em"
+                  width="1em"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2.5"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  className="icon icon-tabler icons-tabler-outline icon-tabler-x">
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                  <path d="M18 6l-12 12" />
+                  <path d="M6 6l12 12" />
+                </svg>
+              </button>
+            </header>
+            <nav className="absolute z-10 top-0 bottom-0 left-4 right-4 flex items-center justify-between">
+              <button
+                title="Previous image"
+                className="text-white text-opacity-90 hover:text-opacity-100 rounded-sm hover:bg-zinc-300 hover:bg-opacity-10 aspect-square w-7 h-7 text-xl inline-flex items-center justify-center"
+                onClick={() =>
+                  setActiveIndex((prev) => (prev - 1 + images.length) % images.length)
+                }>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  height="1em"
+                  width="1em"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  className="icon icon-tabler icons-tabler-outline icon-tabler-chevron-left">
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                  <path d="M15 6l-6 6l6 6" />
+                </svg>
+              </button>
+              <button
+                title="Next image"
+                className="text-white text-opacity-90 hover:text-opacity-100 rounded-sm hover:bg-zinc-300 hover:bg-opacity-10 aspect-square w-7 h-7 text-xl inline-flex items-center justify-center"
+                onClick={() => setActiveIndex((prev) => (prev + 1) % images.length)}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  height="1em"
+                  width="1em"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  className="icon icon-tabler icons-tabler-outline icon-tabler-chevron-right">
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                  <path d="M9 6l6 6l-6 6" />
+                </svg>
+              </button>
+            </nav>
             <div className="flex-1 relative flex items-center justify-center my-12 mx-6">
               {responsiveImage?.base64 && (
                 <img
@@ -75,19 +149,14 @@ export const Image = ({ url, alt, responsiveImage, width, height, size }: FileFi
                 onLoad={() => setLoading(false)}
               />
             </div>
-            <p className="text-white text-opacity-30 text-xs absolute bottom-4 left-6 flex gap-2.5 items-center">
-              <span>
-                {width}x{height}, {humanReadableSize(size)}
-              </span>
-              {loading && <Loader />}
-            </p>
-            <button
-              className="z-50 absolute top-2.5 right-3 sm:top-3.5 sm:right-4 text-white text-opacity-90 hover:text-opacity-100 rounded-sm hover:bg-zinc-300 hover:bg-opacity-10 aspect-square w-7 h-7 text-2xl"
-              onClick={() => setExpanded(false)}>
-              <span className="relative" style={{ top: -3 }}>
-                &times;
-              </span>
-            </button>
+            <footer>
+              <p className="text-white text-opacity-30 text-xs absolute bottom-4 left-4 flex gap-2.5 items-center">
+                <span>
+                  {width}x{height}, {humanReadableSize(size)}
+                </span>
+                {loading && <Loader />}
+              </p>
+            </footer>
           </div>,
           document.getElementById('modal-portal')!,
         )}
