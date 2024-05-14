8f59f50111f1c37daeeb767019ba8a479ab04442
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2024-05-14 13:23:20 +0200

    refactor: remove manual typing of shiki

--- @types/window.d.ts
+++
@@ -1,9 +0,0 @@
-export declare global {
-  interface Window {
-    shiki: {
-      getHighlighter: (options: { theme?: string; langs?: string[] }) => Promise<{
-        codeToHtml: (code: string, options?: { lang?: string }) => string;
-      }>;
-    };
-  }
-}
