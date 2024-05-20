7ab1c78621656220e4c9e39463a594880484c0fc
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2024-05-20 10:35:36 +0200

    feat: download button

--- src/components/image.tsx
+++ src/components/image.tsx
@@ -106,7 +106,31 @@ export const Image = ({ id, width, height, responsiveImage, url, alt, images }: 
         typeof document !== 'undefined' &&
         createPortal(
           <div className="fixed inset-0 z-50 bg-black bg-opacity-95 backdrop-blur-sm flex">
-            <header className="absolute z-20 top-4 left-4 right-4 flex items-center justify-end">
+            <header className="absolute z-20 top-3 left-3 right-3 flex items-center justify-end gap-2">
+              <a
+                className="text-white text-opacity-90 hover:text-opacity-100 rounded-sm hover:bg-zinc-300 hover:bg-opacity-10 aspect-square w-7 h-7 text-lg inline-flex items-center justify-center"
+                href={`/images${new URL(image.url).pathname}`}
+                title="Download"
+                download
+                target="_blank"
+                rel="noreferrer">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  height="1em"
+                  width="1em"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  className="icon icon-tabler icons-tabler-outline icon-tabler-download">
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                  <path d="M4 17v2a2 2 0 0 0 2 2h12a2 2 0 0 0 2 -2v-2" />
+                  <path d="M7 11l5 5l5 -5" />
+                  <path d="M12 4l0 12" />
+                </svg>
+              </a>
               <button
                 title="Close"
                 className="text-white text-opacity-90 hover:text-opacity-100 rounded-sm hover:bg-zinc-300 hover:bg-opacity-10 aspect-square w-7 h-7 text-lg inline-flex items-center justify-center"
