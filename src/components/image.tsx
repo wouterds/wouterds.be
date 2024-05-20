e96297a1af3bfa60a0eb01470122369802694458
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2024-05-20 11:03:28 +0200

    feat: share button if supported

--- src/components/image.tsx
+++ src/components/image.tsx
@@ -107,6 +107,31 @@ export const Image = ({ id, width, height, responsiveImage, url, alt, images }: 
         createPortal(
           <div className="fixed inset-0 z-50 bg-black bg-opacity-95 backdrop-blur-sm flex">
             <header className="absolute z-20 top-3 left-3 right-3 flex items-center justify-end gap-2">
+              {typeof navigator !== 'undefined' && typeof navigator.share !== 'undefined' && (
+                <button
+                  title="Share"
+                  className="text-white text-opacity-90 hover:text-opacity-100 rounded-sm hover:bg-zinc-300 hover:bg-opacity-10 aspect-square w-7 h-7 text-lg inline-flex items-center justify-center"
+                  onClick={() => navigator.share({ url: window.location.href })}>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    height="1em"
+                    width="1em"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    className="icon icon-tabler icons-tabler-outline icon-tabler-share">
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                    <path d="M6 12m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0" />
+                    <path d="M18 6m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0" />
+                    <path d="M18 18m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0" />
+                    <path d="M8.7 10.7l6.6 -3.4" />
+                    <path d="M8.7 13.3l6.6 3.4" />
+                  </svg>
+                </button>
+              )}
               <a
                 className="text-white text-opacity-90 hover:text-opacity-100 rounded-sm hover:bg-zinc-300 hover:bg-opacity-10 aspect-square w-7 h-7 text-lg inline-flex items-center justify-center"
                 href={`/images${new URL(image.url).pathname}`}
