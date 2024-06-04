cbf24796dafa0a00ae0a490c24d38c6b594b41fe
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2024-06-04 23:07:51 +0200

    fix: dark mode for code component

--- src/components/code.tsx
+++ src/components/code.tsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from 'react';
 
+import { useIsDarkMode } from '~/hooks/use-is-dark-mode';
+
 export interface CodeProps {
   children?: string;
   lang: string;
@@ -7,6 +9,7 @@ export interface CodeProps {
 
 export const Code = (props: CodeProps) => {
   const { children: code, lang } = props;
+  const isDarkMode = useIsDarkMode();
 
   const [highlightedCode, setHighlightedCode] = useState<string>();
   useEffect(() => {
@@ -16,16 +19,18 @@ export const Code = (props: CodeProps) => {
 
     // @ts-expect-error: import from esm.sh to avoid large worker bundle
     import('https://esm.sh/shiki@1.5.2').then(async ({ codeToHtml }) => {
-      setHighlightedCode(await codeToHtml(code, { lang, theme: 'github-light' }));
+      setHighlightedCode(
+        await codeToHtml(code, { lang, theme: isDarkMode ? 'github-dark' : 'github-light' }),
+      );
     });
-  }, [code, lang]);
+  }, [code, isDarkMode, lang]);
 
   if (!code) {
     return null;
   }
 
   return (
-    <pre className="text-slate-600 rounded overflow-x-auto">
+    <pre className="text-slate-700 dark:text-slate-300 rounded overflow-x-auto">
       {highlightedCode ? (
         <code dangerouslySetInnerHTML={{ __html: highlightedCode }} />
       ) : (
