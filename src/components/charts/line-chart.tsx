6a34f8648398bbe0b3963378ff48854debb82eba
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2024-06-04 22:48:04 +0200

    refactor: custom line chart component

---
+++ src/components/charts/line-chart.tsx
@@ -0,0 +1,78 @@
+import clsx from 'clsx';
+import { ReactNode, useMemo } from 'react';
+import { Line, LineChart as Chart, ResponsiveContainer, YAxis } from 'recharts';
+
+import { useIsDarkMode } from '~/hooks/use-is-dark-mode';
+
+type Props = {
+  data: Array<Record<string, unknown>>;
+  dataKey: string;
+  label: string;
+  header?: string;
+  unit: string;
+  className?: string;
+  compact?: boolean;
+  footer?: ReactNode[];
+  scale?: { min: number; max: number };
+};
+
+export const LineChart = ({
+  data,
+  scale,
+  dataKey,
+  unit,
+  compact,
+  label,
+  header,
+  className,
+  footer,
+}: Props) => {
+  const isDarkMode = useIsDarkMode();
+  const chartColor = useMemo(() => (isDarkMode ? '#fff' : '#000'), [isDarkMode]);
+  const filteredComponents = footer?.filter(Boolean);
+
+  return (
+    <>
+      <div className={clsx('border border-black dark:border-white text-center', className)}>
+        <div className="py-2">
+          <span className="font-semibold">
+            {header ||
+              `${((data?.[data.length - 1]?.[dataKey] as number) || 0)?.toFixed(2)} ${unit}`}
+          </span>
+        </div>
+        <div
+          className={clsx('relative -my-1', {
+            'aspect-[8/1] sm:aspect-[10/1]': !compact,
+            'aspect-[4/1]': compact,
+          })}>
+          <ResponsiveContainer>
+            <Chart data={data}>
+              <Line dataKey={dataKey} stroke={chartColor} strokeWidth={1.5} dot={false} />
+              <YAxis
+                hide
+                domain={
+                  scale
+                    ? [
+                        Math.min(...data.map((record) => record[dataKey] as number)) * scale.min,
+                        Math.max(...data.map((record) => record[dataKey] as number)) * scale.max,
+                      ]
+                    : undefined
+                }
+              />
+            </Chart>
+          </ResponsiveContainer>
+        </div>
+        <div
+          className="font-medium bg-black dark:bg-white text-white dark:text-black py-0.5"
+          style={{ margin: 1 }}>
+          {label}
+        </div>
+      </div>
+      {filteredComponents && (
+        <p className="flex flex-col sm:flex-row gap-1 justify-start sm:justify-between mt-2">
+          {filteredComponents}
+        </p>
+      )}
+    </>
+  );
+};
