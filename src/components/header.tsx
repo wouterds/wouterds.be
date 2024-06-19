99f9aeb670396713e19f462edfea6aa3be3cea2f
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2024-06-19 19:30:40 +0200

    feat: now playing

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,30 +1,35 @@
 import { Link } from '@remix-run/react';
 
+import { NowPlaying } from './now-playing';
+
 const Header = () => (
-  <header className="border-t border-b border-dashed border-zinc-900 dark:border-zinc-100 py-3">
-    <ul className="flex justify-end items-center gap-2.5">
-      <li>
-        <Link to="/" prefetch="intent">
-          /
-        </Link>
-      </li>
-      <li>
-        <Link to="/about" prefetch="intent">
-          about
-        </Link>
-      </li>
-      <li>
-        <Link to="/blog" prefetch="intent">
-          blog
-        </Link>
-      </li>
-      <li>
-        <Link to="/contact" prefetch="intent">
-          contact
-        </Link>
-      </li>
-    </ul>
-  </header>
+  <>
+    <NowPlaying />
+    <header className="border-t border-b border-dashed border-zinc-900 dark:border-zinc-100 py-3">
+      <ul className="flex justify-end items-center gap-2.5">
+        <li>
+          <Link to="/" prefetch="intent">
+            /
+          </Link>
+        </li>
+        <li>
+          <Link to="/about" prefetch="intent">
+            about
+          </Link>
+        </li>
+        <li>
+          <Link to="/blog" prefetch="intent">
+            blog
+          </Link>
+        </li>
+        <li>
+          <Link to="/contact" prefetch="intent">
+            contact
+          </Link>
+        </li>
+      </ul>
+    </header>
+  </>
 );
 
 export default Header;
