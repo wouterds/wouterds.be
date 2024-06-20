db87e9bdd06e43d7a40e973ea10b021a66a9b4d7
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2024-06-20 22:53:21 +0200

    fix: diff

--- src/routes/api.experiments.ts
+++ src/routes/api.experiments.ts
@@ -15,7 +15,7 @@ export const loader = async ({ context }: LoaderFunctionArgs) => {
   let track: SpotifyTrack | null = await spotify.getCurrentlyPlaying();
   if (!track) {
     const recentlyPlayed = await spotify.getRecentlyPlayed(1)?.then((tracks) => tracks?.[0]);
-    if (recentlyPlayed && differenceInMinutes(new Date(), recentlyPlayed.playedAt) > 10) {
+    if (recentlyPlayed && differenceInMinutes(recentlyPlayed.playedAt, new Date()) > 10) {
       track = recentlyPlayed;
     }
   }
