19a33f968038070367f1d8325ebee112a930e024
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2024-07-13 18:55:53 +0200

    test: add test for excerptFromContent

--- src/lib/datocms.spec.ts
+++ src/lib/datocms.spec.ts
@@ -1,10 +1,11 @@
 import { StructuredText } from 'datocms-structured-text-utils';
 
-import { plainTextFromContent } from './datocms';
+import { excerptFromContent, plainTextFromContent } from './datocms';
 
 describe('datocms', () => {
   describe('plainTextFromContent', () => {
     it('should return plain text from content', () => {
+      // given
       const content = {
         schema: 'dast',
         document: {
@@ -24,7 +25,50 @@ describe('datocms', () => {
         },
       } as unknown as StructuredText;
 
-      expect(plainTextFromContent(content)).toBe('Hello, world!');
+      // when
+      const result = plainTextFromContent(content);
+
+      // then
+      expect(result).toEqual('Hello, world!');
+    });
+  });
+
+  describe('excerptFromContent', () => {
+    it('should return excerpt from content', () => {
+      // given
+      const content = {
+        schema: 'dast',
+        document: {
+          type: 'root',
+          children: [
+            {
+              type: 'heading',
+              level: 1,
+              children: [
+                {
+                  type: 'span',
+                  value: 'Hello, world!',
+                },
+              ],
+            },
+            {
+              type: 'paragraph',
+              children: [
+                {
+                  type: 'span',
+                  value: 'This is test content.',
+                },
+              ],
+            },
+          ],
+        },
+      } as unknown as StructuredText;
+
+      // when
+      const result = excerptFromContent(content);
+
+      // then
+      expect(result).toEqual('Hello, world!\nThis is test…');
     });
   });
 });
