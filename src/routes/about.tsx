922a4d45e9d2d8b1080c540f3c0a2497a86c9105
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2024-07-20 12:30:22 +0200

    feat: change about copy

--- src/routes/about.tsx
+++ src/routes/about.tsx
@@ -1,61 +1,31 @@
 import { MetaFunction } from '@remix-run/cloudflare';
-import { differenceInMilliseconds, differenceInYears } from 'date-fns';
-import { useState } from 'react';
-
-import { useInterval } from '~/hooks/use-interval';
-
-const BIRTHDAY = '13 December 1992';
-
-const getAge = () => {
-  return (
-    differenceInMilliseconds(new Date(), new Date(BIRTHDAY)) /
-    (365 * 24 * 60 * 60) /
-    1000
-  ).toFixed(8);
-};
 
 export const meta: MetaFunction = () => {
   return [
     { title: 'About' },
     {
       name: 'description',
-      content: `Hey 👋, I'm Wouter, ${differenceInYears(
-        new Date(),
-        BIRTHDAY,
-      )} years old and passionate about all things digital really. Devine alumni & currently contracting as a Full-stack Developer.`,
+      content: `Hi, I&apos;m Wouter, a Full-stack Developer with over 10 years of experience in web and mobile development. I specialize in React, React Native, and Node.js. I studied Digital Design & Development at Howest University College and have worked with various startups, scale-ups, and agencies.`,
     },
   ];
 };
 
 export default function About() {
-  const [age, setAge] = useState(getAge());
-  useInterval(() => setAge(getAge()), 1);
-
   return (
     <div className="leading-relaxed">
       <p className="mb-4">
-        Hey 👋, I&apos;m Wouter, {age} years old and passionate about all things digital really.
+        Hi, I&apos;m Wouter, a Full-stack Developer with over 10 years of experience in web and
+        mobile development. I specialize in React, React Native, and Node.js. I studied Digital
+        Design & Development at Howest University College and have worked with various startups,
+        scale-ups, and agencies.
       </p>
       <p className="mb-4">
-        I studied Digital Design & Development at Howest University College and currently I&apos;m
-        contracting as Full-stack Developer. I have over 10 years of experience as a professional
-        web & mobile developer and the last few years, I&apos;ve been specializing in React, React
-        Native & Node.js (web) applications. Before I started contracting I used to work for a
-        variety of startups, scale-ups and agencies.
+        In my free time I enjoy experimenting with Arduino, ESP, and Raspberry Pi, and electronics
+        in general. Also actively been following the crypto space since 2017.
       </p>
       <p className="mb-4">
-        When I&apos;m not creating kickass digital applications, I&apos;m probably fiddling around
-        with electronics using Arduino, ESP and Raspberry Pi. The restless mind that I am wants to
-        learn and expand knowledge all the time. Besides that, I really love traveling, trying new
-        things and pushing my own limits. Also kind of a data geek, charts & stats are real
-        motivators for me.
+        I&apos;m passionate about continuous learning, traveling, and data analytics.
       </p>
-      <p className="mb-4">
-        Since 2017 involved in the crypto space and really started to appreciate the power of
-        decentralized applications. And to be honest, it&apos;s probably one of the most exciting
-        things I&apos;ve come across lately!
-      </p>
-      <p className="mb-4">Also a cat person! 🐱</p>
     </div>
   );
 }
