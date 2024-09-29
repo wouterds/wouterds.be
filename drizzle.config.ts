36f3da5a0f4bd0d5dbf6b4758a15080a6cd274b9
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2024-09-29 17:06:39 +0200

    chore(database): migration scripts

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,4 +4,11 @@ export default defineConfig({
   dialect: 'mysql',
   schema: './src/database/models',
   out: './src/database/migrations',
+  dbCredentials: {
+    host: 'todo',
+    port: 3306,
+    user: 'todo',
+    password: 'todo',
+    database: 'todo',
+  },
 });
