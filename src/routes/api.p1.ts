29d1be91649bb6b2722e303adadce4a232d89b67
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2024-10-05 10:42:23 +0200

    refactor: store p1 readings using database

--- src/routes/api.p1.ts
+++ src/routes/api.p1.ts
@@ -1,72 +1,38 @@
 import { ActionFunctionArgs, json } from '@remix-run/node';
-import { differenceInMinutes, endOfYesterday, fromUnixTime, getUnixTime } from 'date-fns';
+import { differenceInMinutes } from 'date-fns';
 
-import { P1HistoryRecord, P1Record } from '~/data/repositories/p1-repository';
+import { P1Readings } from '~/database/p1-readings/repository';
 
-export const action = async ({ request, context }: ActionFunctionArgs) => {
-  const query = new URL(request.url).searchParams;
-  if (query.get('token') !== process.env.API_AUTH_TOKEN) {
+export const action = async ({ request }: ActionFunctionArgs) => {
+  if (request.headers.get('authorization') !== process.env.API_AUTH_TOKEN) {
     return json({ success: false }, { status: 403 });
   }
 
-  const body = (await request.text()) || '';
-  if (!body) {
+  const data = await request.json().catch(() => null);
+  if (!data) {
     return json({ success: false }, { status: 400 });
   }
 
-  const data: Array<{
-    active: number;
-    total: number;
-    peak: number;
-    peak_timestamp: number;
-  }> = JSON.parse(`[${body.split('}{').join('},{')}]`);
-
-  const active = Math.round(data.reduce((acc, curr) => acc + curr.active, 0) / data.length);
-  const total = data[data.length - 1].total;
-  const peak = data[data.length - 1].peak;
-  const [year, month, day, hour, minute] =
-    `${data[data.length - 1].peak_timestamp}`.match(/.{1,2}/g)?.flat() || [];
-  const peakTime = getUnixTime(new Date(`20${year}-${month}-${day} ${hour}:${minute}`));
-  const time = getUnixTime(new Date());
-
-  const raw = await process.env.CACHE.get('p1');
-  const values: Omit<P1Record, 'peak_timestamp'>[] = raw ? JSON.parse(raw) : [];
-
-  const lastPush = fromUnixTime(values[values.length - 1]?.time ?? 0);
-  if (differenceInMinutes(new Date(), lastPush) < 10) {
+  const lastPush = await P1Readings.getLast();
+  if (lastPush && differenceInMinutes(new Date(), lastPush.created_at) < 10) {
     return json({ success: false }, { status: 429 });
   }
 
-  values.push({ active, total, time });
-
-  // readings are every 10 minutes, so keep 24 hours worth of data
-  if (values.length > (24 * 60) / 10) {
-    values.shift();
+  const active = data.active_power_w;
+  const total = data.total_power_import_kwh;
+  const peak = data.montly_power_peak_w;
+  const time = `${data.montly_power_peak_timestamp}`.match(/.{1,2}/g)?.flat() || [];
+  if (time?.length < 5) {
+    return json({ success: false }, { status: 400 });
   }
+  const peakedAt = new Date(`20${time[0]}-${time[1]}-${time[2]} ${time[3]}:${time[4]}`);
 
-  await process.env.CACHE.put('p1', JSON.stringify(values));
-
-  const rawHistory = await process.env.CACHE.get('p1-history');
-  const history: P1HistoryRecord[] = rawHistory ? JSON.parse(rawHistory) : [];
-  const lastHistoryRecord = history[history.length - 1];
-
-  const yesterday = getUnixTime(endOfYesterday());
-
-  if (
-    // if there's no history yet
-    !lastHistoryRecord ||
-    // or the last entry is not yesterday
-    yesterday !== lastHistoryRecord.time
-  ) {
-    history.push({
-      total,
-      peak,
-      peakTime,
-      time: yesterday,
-    });
-
-    await process.env.CACHE.put('p1-history', JSON.stringify(history));
-  }
+  await P1Readings.add({
+    active,
+    total,
+    peak,
+    peaked_at: peakedAt,
+  });
 
   return { success: true };
 };
