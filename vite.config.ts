4d999267eb4f3db05e472675f1dceef7c1af6072
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2024-10-06 22:45:45 +0200

    chore: opt-in into optimizeDeps

--- vite.config.ts
+++ vite.config.ts
@@ -18,6 +18,7 @@ export default defineConfig({
         v3_fetcherPersist: true,
         v3_relativeSplatPath: true,
         v3_throwAbortReason: true,
+        unstable_optimizeDeps: true,
       },
     }),
     tsconfigPaths(),
