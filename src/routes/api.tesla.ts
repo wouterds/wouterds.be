4de472f7cd4bf01264b96a177db56af16e2c2490
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2024-10-06 11:19:36 +0200

    refactor(tesla-data): migrate tesla endpoint to use mysql

--- src/routes/api.tesla.ts
+++ src/routes/api.tesla.ts
@@ -1,24 +1,24 @@
-import { json, LoaderFunctionArgs } from '@remix-run/node';
-import { differenceInMinutes, fromUnixTime, getUnixTime } from 'date-fns';
+import { json } from '@remix-run/node';
+import { differenceInMinutes } from 'date-fns';
 
-import { TeslaRepository } from '~/data/repositories/tesla-repository';
+import { TeslaData } from '~/database/tesla-data/repository';
 import { Tesla } from '~/lib/tesla';
 
 const SYNC_INTERVAL_MINUTES = 15; // 15 minutes
 const WAKE_INTERVAL_MINUTES = 60 * 2; // 2 hours
 const VIN = 'LRW3E7EKXMC324303';
 
-export const loader = async ({ context }: LoaderFunctionArgs) => {
-  const last = await TeslaRepository.create(context).getLast();
-  if (differenceInMinutes(new Date(), fromUnixTime(last?.time || 0)) < SYNC_INTERVAL_MINUTES) {
+export const loader = async () => {
+  const last = await TeslaData.getLast();
+  if (last && differenceInMinutes(new Date(), last.created_at) < SYNC_INTERVAL_MINUTES) {
     return json(last, { status: 429 });
   }
 
-  const tesla = await Tesla.create(context).setVin(VIN).auth();
+  const tesla = new Tesla().setVin(VIN);
 
   let data = await tesla.getData();
   const isSleeping = data?.error?.includes('offline or asleep');
-  const lastAwake = fromUnixTime((await TeslaRepository.create(context).getLastAwake())?.time || 0);
+  const lastAwake = (await TeslaData.getLastAwake()).created_at || new Date(0);
   if (isSleeping && differenceInMinutes(new Date(), lastAwake) > WAKE_INTERVAL_MINUTES) {
     await tesla.wakeUp();
     data = await tesla.getData();
@@ -29,16 +29,13 @@ export const loader = async ({ context }: LoaderFunctionArgs) => {
   }
 
   const response = data?.response;
-  const record = await TeslaRepository.create(context).add({
+  const record = await TeslaData.add({
     battery: (response?.charge_state?.battery_level || last?.battery) as number,
     distance: parseFloat(
       (((response?.vehicle_state?.odometer || 0) * 1.60934 || last?.distance) as number).toFixed(3),
     ),
-    temperature: {
-      inside: response?.climate_state?.inside_temp || (last?.temperature.inside as number),
-      outside: response?.climate_state?.outside_temp || (last?.temperature.outside as number),
-    },
-    time: getUnixTime(new Date()),
+    temperature_inside: response?.climate_state?.inside_temp || last?.temperature_inside,
+    temperature_outside: response?.climate_state?.outside_temp || last?.temperature_outside,
     wake: !!response,
   });
 
