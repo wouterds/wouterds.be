6efa59cd557f8216f383b606fb593121102d5200
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2024-10-06 22:48:53 +0200

    chore: opt-in into single fetch

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,12 @@ import { sentryVitePlugin } from '@sentry/vite-plugin';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
+declare module '@remix-run/node' {
+  interface Future {
+    unstable_singleFetch: true;
+  }
+}
+
 export default defineConfig({
   build: {
     target: 'esnext',
@@ -19,6 +25,7 @@ export default defineConfig({
         v3_relativeSplatPath: true,
         v3_throwAbortReason: true,
         unstable_optimizeDeps: true,
+        unstable_singleFetch: true,
       },
     }),
     tsconfigPaths(),
