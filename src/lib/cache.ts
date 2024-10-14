06a690f8e2618b4b3079937604b4b90cc8afd747
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2024-10-14 21:40:48 +0200

    feat: add some cache logs

--- src/lib/cache.ts
+++ src/lib/cache.ts
@@ -1,4 +1,7 @@
 import { createClient, RedisClientType } from '@redis/client';
+import { formatDistanceStrict, formatDistanceToNowStrict } from 'date-fns';
+
+import { logger } from './logger';
 
 const REDIS_HOST = process.env.REDIS_HOST || 'redis';
 
@@ -20,20 +23,27 @@ class RedisCache {
   }
 
   public get = async (key: string) => {
+    logger.wait(`[${key}]`, 'checking cache');
+
     const value = await this.client.get(key);
     if (value) {
-      console.log('cache hit', key);
+      const ttl = await this.client.ttl(key);
+      const expiresIn = ttl > 0 ? formatDistanceStrict(0, ttl * 1000) : null;
+
+      logger.event(`[${key}]`, `cache hit${expiresIn ? `, expires in ${expiresIn}` : ''}`);
 
       return JSON.parse(value);
     }
 
-    console.log('cache miss', key);
+    logger.warn(`[${key}]`, 'cache miss');
 
     return null;
   };
 
   public set = async <T = unknown>(key: string, value: T, expiry?: Date) => {
-    console.log('cache set', key);
+    const diff = expiry ? formatDistanceToNowStrict(expiry) : null;
+
+    logger.event(`[${key}]`, `cache set${diff ? `, expires in ${diff}` : ''}`);
 
     return this.client.set(key, JSON.stringify(value), {
       EX: expiry ? Math.floor((expiry.getTime() - Date.now()) / 1000) : undefined,
