4e4e65ee2961298cd558d23619db2be33e6ad484
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2024-10-15 22:35:40 +0200

    build: disable single fetch

--- vite.config.ts
+++ vite.config.ts
@@ -25,7 +25,7 @@ export default defineConfig({
         v3_relativeSplatPath: true,
         v3_throwAbortReason: true,
         unstable_optimizeDeps: true,
-        v3_singleFetch: true,
+        v3_singleFetch: false,
       },
     }),
     tsconfigPaths(),
