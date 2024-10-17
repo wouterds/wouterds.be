085f95e49dac1dc145952a6a5c385633fc13b3c8
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2024-10-17 22:55:00 +0200

    refactor: create mysql connection without top level await

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,7 +1,7 @@
 import { drizzle } from 'drizzle-orm/mysql2';
-import mysql from 'mysql2/promise';
+import mysql from 'mysql2';
 
-const connection = await mysql.createConnection({
+const connection = mysql.createConnection({
   host: process.env.DB_HOST!,
   port: 3306,
   user: process.env.DB_USER!,
