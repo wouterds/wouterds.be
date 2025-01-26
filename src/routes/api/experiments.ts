514351c998cc4ddd1035ab3cdc4dde0517ad7a79
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2025-01-26 23:08:35 +0100

    feat: extend experiments api

--- src/routes/api/experiments.ts
+++ src/routes/api/experiments.ts
@@ -1,9 +1,29 @@
 import { differenceInMinutes } from 'date-fns';
 
-import { AranetReadings, TeslaData } from '~/database';
+import { AranetReadings, NUCReadings, P1Readings, TeslaData } from '~/database';
 import { Spotify } from '~/lib/spotify.server';
 
 export const loader = async () => {
+  const [aranet, tesla, p1, nuc, spotify] = await Promise.all([
+    AranetReadings.getLast(),
+    TeslaData.getLast(),
+    P1Readings.getLast(),
+    NUCReadings.getLast(),
+    getLastSpotifyTrack(),
+  ]);
+
+  return Response.json(
+    { aranet, tesla, p1, nuc, spotify },
+    {
+      headers: {
+        'Cache-Control': 'public, max-age=30, must-revalidate',
+        'Access-Control-Allow-Origin': '*',
+      },
+    },
+  );
+};
+
+const getLastSpotifyTrack = async () => {
   const spotify = new Spotify();
 
   let track = await spotify.getCurrentlyPlaying();
@@ -14,17 +34,5 @@ export const loader = async () => {
     }
   }
 
-  return Response.json(
-    {
-      aranet: await AranetReadings.getLast(),
-      tesla: await TeslaData.getLast(),
-      spotify: track,
-    },
-    {
-      headers: {
-        'Cache-Control': 'public, max-age=60, must-revalidate',
-        'Access-Control-Allow-Origin': '*',
-      },
-    },
-  );
+  return track;
 };
