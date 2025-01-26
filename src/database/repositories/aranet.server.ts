0c274d9f3037db2fafdb757b9e51542f171f5173
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2025-01-26 22:36:03 +0100

    refactor: remove unused repo methods

--- src/database/repositories/aranet.server.ts
+++ src/database/repositories/aranet.server.ts
@@ -1,5 +1,4 @@
-import { subHours } from 'date-fns';
-import { asc, desc, gte, sql } from 'drizzle-orm';
+import { desc } from 'drizzle-orm';
 
 import { db } from '~/database/connection.server';
 
@@ -7,48 +6,6 @@ import { AranetReading } from '../schema/aranet.server';
 
 const add = async (data: AranetReading) => {
   await db.insert(AranetReading).values(data);
-};
-
-const getAll = async (limit?: number) => {
-  const query = db.select().from(AranetReading);
-  if (limit) {
-    query.limit(limit);
-  }
-
-  return query.orderBy(desc(AranetReading.createdAt));
-};
-
-const getLast24h = async (options?: { sort: 'asc' | 'desc' }) => {
-  const sort = options?.sort ?? 'desc';
-
-  const rows = await db
-    .select({
-      timeGroup: sql<string>`
-        DATE_FORMAT(
-          DATE_SUB(
-            ${AranetReading.createdAt},
-            INTERVAL MINUTE(${AranetReading.createdAt}) % 20 MINUTE
-          ),
-          '%Y-%m-%d %H:%i:00'
-        )
-      `.as('timeGroup'),
-      avgCo2: sql<string>`ROUND(AVG(${AranetReading.co2}), 0)`,
-      avgTemperature: sql<string>`ROUND(AVG(${AranetReading.temperature}), 1)`,
-      avgHumidity: sql<string>`ROUND(AVG(${AranetReading.humidity}), 1)`,
-      avgPressure: sql<string>`ROUND(AVG(${AranetReading.pressure}), 1)`,
-    })
-    .from(AranetReading)
-    .where(gte(AranetReading.createdAt, subHours(new Date(), 24)))
-    .groupBy(sql`timeGroup`)
-    .orderBy(sort === 'desc' ? desc(sql`timeGroup`) : asc(sql`timeGroup`));
-
-  return rows.map((row) => ({
-    co2: parseFloat(row.avgCo2),
-    temperature: parseFloat(row.avgTemperature),
-    humidity: parseFloat(row.avgHumidity),
-    pressure: parseFloat(row.avgPressure),
-    timestamp: new Date(row.timeGroup),
-  }));
 };
 
 const getLast = async () => {
@@ -67,8 +24,6 @@ const truncate = async () => {
 
 export const AranetReadings = {
   add,
-  getAll,
-  getLast24h,
   getLast,
   truncate,
 };
--- src/database/repositories/p1.server.ts
+++ src/database/repositories/p1.server.ts
@@ -1,5 +1,4 @@
-import { subHours } from 'date-fns';
-import { asc, desc, gte, sql } from 'drizzle-orm';
+import { desc } from 'drizzle-orm';
 
 import { db } from '~/database/connection.server';
 
@@ -7,44 +6,6 @@ import { P1Reading } from '../schema/p1.server';
 
 const add = async (data: P1Reading) => {
   await db.insert(P1Reading).values(data);
-};
-
-const getAll = async (limit?: number) => {
-  const query = db.select().from(P1Reading);
-  if (limit) {
-    query.limit(limit);
-  }
-
-  return query.orderBy(desc(P1Reading.createdAt));
-};
-
-const getLast24h = async (options?: { sort: 'asc' | 'desc' }) => {
-  const sort = options?.sort ?? 'desc';
-
-  const rows = await db
-    .select({
-      timeGroup: sql<string>`
-      DATE_FORMAT(
-        DATE_SUB(
-          ${P1Reading.createdAt},
-          INTERVAL MINUTE(${P1Reading.createdAt}) % 10 MINUTE
-        ),
-        '%Y-%m-%d %H:%i:00'
-      )
-    `.as('timeGroup'),
-      avgActive: sql<string>`ROUND(AVG(${P1Reading.active}), 0)`,
-      avgTotal: sql<string>`ROUND(AVG(${P1Reading.total}), 1)`,
-    })
-    .from(P1Reading)
-    .where(gte(P1Reading.createdAt, subHours(new Date(), 24)))
-    .groupBy(sql`timeGroup`)
-    .orderBy(sort === 'desc' ? desc(sql`timeGroup`) : asc(sql`timeGroup`));
-
-  return rows.map((row) => ({
-    active: parseFloat(row.avgActive),
-    total: parseFloat(row.avgTotal),
-    timestamp: new Date(row.timeGroup),
-  }));
 };
 
 const getLast = async () => {
@@ -59,8 +20,6 @@ const truncate = async () => {
 
 export const P1Readings = {
   add,
-  getAll,
-  getLast24h,
   getLast,
   truncate,
 };
