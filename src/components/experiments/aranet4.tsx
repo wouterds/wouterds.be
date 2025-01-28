c188ab544de45d3106dc9edd7c6d4a8f5fec55e8
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2025-01-28 22:52:08 +0100

    feat: aranet battery icon

--- src/components/experiments/aranet4.tsx
+++ src/components/experiments/aranet4.tsx
@@ -1,4 +1,13 @@
-import { Droplets, Gauge, Thermometer, Wind } from 'lucide-react';
+import {
+  Battery,
+  BatteryFull,
+  BatteryLow,
+  BatteryMedium,
+  Droplets,
+  Gauge,
+  Thermometer,
+  Wind,
+} from 'lucide-react';
 
 import { ExperimentCard } from './experiment-card';
 import { MetricItem } from './metric-item';
@@ -9,7 +18,16 @@ type Props = {
     humidity: number;
     co2: number;
     pressure: number;
+    battery: number;
   };
+};
+
+const getBatteryIcon = (percentage?: number) => {
+  if (percentage == null) return Battery;
+  if (percentage < 15) return Battery;
+  if (percentage < 30) return BatteryLow;
+  if (percentage < 70) return BatteryMedium;
+  return BatteryFull;
 };
 
 export const Aranet4 = ({ data }: Props) => {
@@ -19,6 +37,7 @@ export const Aranet4 = ({ data }: Props) => {
       <MetricItem icon={Droplets} value={data?.humidity} unit="%" />
       <MetricItem icon={Wind} value={data?.co2} unit="ppm" />
       <MetricItem icon={Gauge} value={data?.pressure} unit="hPa" />
+      <MetricItem icon={getBatteryIcon(data?.battery)} value={data?.battery} unit="%" />
     </ExperimentCard>
   );
 };
