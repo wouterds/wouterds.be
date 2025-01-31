27f3e9d1ecd5b1e60e358a32c0684fedcca8f28a
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2025-01-31 11:16:38 +0100

    chore: disable charts

--- src/components/experiments/index.tsx
+++ src/components/experiments/index.tsx
@@ -5,10 +5,10 @@ import { NUC } from './cards/nuc';
 import { Power } from './cards/power';
 import { SpotifyNowPlaying } from './cards/spotify-now-playing';
 import { Tesla } from './cards/tesla';
-import { Aranet4Charts } from './charts/aranet4-charts';
-import { NUCCharts } from './charts/nuc-charts';
-import { PowerCharts } from './charts/power-charts';
-import { TeslaCharts } from './charts/tesla-charts';
+// import { Aranet4Charts } from './charts/aranet4-charts';
+// import { NUCCharts } from './charts/nuc-charts';
+// import { PowerCharts } from './charts/power-charts';
+// import { TeslaCharts } from './charts/tesla-charts';
 
 type Data = {
   aranet: {
@@ -49,7 +49,6 @@ type Data = {
 
 export const Experiments = () => {
   const [data, setData] = useState<Data | null>(null);
-  const [activeExperiment, setActiveExperiment] = useState<string | null>();
 
   const fetchData = async () => {
     const response = await fetch('/api/experiments');
@@ -68,39 +67,16 @@ export const Experiments = () => {
 
   return (
     <div className="mt-12 text-gray-800">
-      {activeExperiment === 'aranet' && <Aranet4Charts />}
-      {activeExperiment === 'tesla' && <TeslaCharts />}
-      {activeExperiment === 'p1' && <PowerCharts />}
-      {activeExperiment === 'nuc' && <NUCCharts />}
-
+      {/* <Aranet4Charts />
+      <TeslaCharts />
+      <PowerCharts />
+      <NUCCharts /> */}
       <div className="flex flex-nowrap whitespace-nowrap text-nowrap bg-gradient-to-b from-gray-100 to-white min-w-full px-3 sm:px-5 py-2 text-sm overflow-x-auto border-t border-gray-200">
-        <div
-          onMouseEnter={() => setActiveExperiment('aranet')}
-          onMouseLeave={() => setActiveExperiment(null)}>
-          <Aranet4 data={data?.aranet} />
-        </div>
-        <div
-          onMouseEnter={() => setActiveExperiment('tesla')}
-          onMouseLeave={() => setActiveExperiment(null)}>
-          <Tesla data={data?.tesla} />
-        </div>
-        <div
-          onMouseEnter={() => setActiveExperiment('p1')}
-          onMouseLeave={() => setActiveExperiment(null)}>
-          <Power data={data?.p1} />
-        </div>
-        <div
-          onMouseEnter={() => setActiveExperiment('nuc')}
-          onMouseLeave={() => setActiveExperiment(null)}>
-          <NUC data={data?.nuc} />
-        </div>
-        {data?.spotify && (
-          <div
-            onMouseEnter={() => setActiveExperiment('spotify')}
-            onMouseLeave={() => setActiveExperiment(null)}>
-            <SpotifyNowPlaying data={data.spotify} />
-          </div>
-        )}
+        <Aranet4 data={data?.aranet} />
+        <Tesla data={data?.tesla} />
+        <Power data={data?.p1} />
+        <NUC data={data?.nuc} />
+        {data?.spotify && <SpotifyNowPlaying data={data.spotify} />}
       </div>
     </div>
   );
