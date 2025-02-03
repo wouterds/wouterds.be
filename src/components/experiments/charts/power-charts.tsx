f3ff3cac64449fe792d127e237e2614a1929bd52
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2025-02-03 20:34:46 +0100

    fix: change power scale

--- src/components/experiments/charts/power-charts.tsx
+++ src/components/experiments/charts/power-charts.tsx
@@ -1,8 +1,6 @@
 import { BaseCharts } from './base-charts';
 
-const dataConfigs = [
-  { dataKey: 'active', unit: 'W', label: 'power usage', scale: { min: 0.9, max: 1 } },
-];
+const dataConfigs = [{ dataKey: 'active', unit: 'W', label: 'power usage' }];
 
 export const PowerCharts = () => {
   return <BaseCharts dataConfigs={dataConfigs} apiEndpoint="/api/experiments?timeseries=p1" />;
